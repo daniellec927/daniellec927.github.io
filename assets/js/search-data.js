22a4f4551c0236e307c14c35df7bad18df388e34
daniellec927 <147299272+daniellec927@users.noreply.github.com>
2026-08-05 10:41:06 +0000

    Deploying to gh-pages from @ daniellec927/daniellec927.github.io@dc86ef14dfd78f4cfa322251e37604e1431c8bdc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,31 +408,16 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-06-01-example/";
+            },},{id: "projects-2026-march-madness-prediction",
+          title: '2026 March Madness Prediction',
+          description: "ML model that predicts the outcome of NCAA Men&#39;s and Women&#39;s March Madness basketball tournament games",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/march_madness_project/";
             },},{id: "projects-todo-project-name",
           title: 'TODO — project name',
           description: "TODO — one line on what it does and why it matters",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-todo-project-name",
-          title: 'TODO — project name',
-          description: "TODO — one line on what it does and why it matters",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-todo-project-name",
-          title: 'TODO — project name',
-          description: "TODO — one line on what it does and why it matters",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-todo-project-name",
-          title: 'TODO — project name',
-          description: "TODO — one line on what it does and why it matters",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-todo-project-name",
-          title: 'TODO — project name',
-          description: "TODO — one line on what it does and why it matters",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/projects/sample_project/";
             },},{
         id: 'social-cv',
         title: 'CV',
